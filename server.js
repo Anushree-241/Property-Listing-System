e9e3c0c0459690393f08498cff0f20dc8134d6d3
Anushree .R <91838443+Anushree-241@users.noreply.github.com>
2025-05-31 21:09:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(()=>console.log('MongoDB Connected!'))
 .catch((err)=>{
     console.error('MongoDB connecion error:',err);
